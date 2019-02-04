6fd66ea6c98a7b341399382578da9fe3c105d08b
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-02-05 04:34:47 +0800

    fix: bugfix

--- aimdfragmentation/test/test_fragment.py
+++ aimdfragmentation/test/test_fragment.py
@@ -9,20 +9,15 @@ from aimdfragmentation import AIMDFragmentation
 
 class Test_all(unittest.TestCase):
     def test_fragment(self):
-        folder = "testfiles"
         xyzfilename = 'test_fragment.xyz'
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        with open(os.path.join(folder, xyzfilename), 'wb') as f:
-            f.write(pkg_resources.resource_string(__name__, 'test.xyz'))
+        with open(xyzfilename, 'w') as f:
+            f.write(pkg_resources.resource_string(__name__, 'test.xyz').decode())
         af = AIMDFragmentation(cutoff=6.0, xyzfilename=xyzfilename)
         af.run()
-        with open(af.outputenergyfile) as f:
-            logging.info("Energy:")
-            print(f.read())
-        with open(af.outputenergyfile) as f:
-            logging.info("Force:")
-            print(f.read())
+        for filename in (af.outputenergyfile, af.outputfile):
+            with open(filename) as f:
+                logging.info(filename)
+                print(f.read())
 
 
 if __name__ == '__main__':
