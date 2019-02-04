37411071a81a21432753aac4a26fd4d25c7c7f81
stickler-ci <support@stickler-ci.com>
2019-02-04 20:35:23 +0000

    Fixing style errors.

--- aimdfragmentation/test/test_fragment.py
+++ aimdfragmentation/test/test_fragment.py
@@ -11,7 +11,8 @@ class Test_all(unittest.TestCase):
     def test_fragment(self):
         xyzfilename = 'test_fragment.xyz'
         with open(xyzfilename, 'w') as f:
-            f.write(pkg_resources.resource_string(__name__, 'test.xyz').decode())
+            f.write(pkg_resources.resource_string(
+                __name__, 'test.xyz').decode())
         af = AIMDFragmentation(cutoff=6.0, xyzfilename=xyzfilename)
         af.run()
         for filename in (af.outputenergyfile, af.outputfile):
