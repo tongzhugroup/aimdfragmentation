4afacc7200e6a6791fc6b1e85eed2bc1f6599c71
Jinzhe Zeng <njzjz@qq.com>
2019-02-06 03:55:37 +0800

    fix: test

--- aimdfragmentation/test/test_fragment.py
+++ aimdfragmentation/test/test_fragment.py
@@ -1,25 +1,24 @@
 import os
-import unittest
 import logging
+import tempfile
 
 import pkg_resources
 
 from aimdfragmentation import AIMDFragmentation
 
 
-class Test_all(unittest.TestCase):
+class Test_all:
     def test_fragment(self):
-        xyzfilename = 'test_fragment.xyz'
+        folder = tempfile.mkdtemp(prefix='testfiles-', dir='.')
+        logging.info(f'Folder: {folder}:')
+        os.chdir(folder)
+        xyzfilename = 'test.xyz'
         with open(xyzfilename, 'w') as f:
             f.write(pkg_resources.resource_string(
-                __name__, 'test.xyz').decode())
+                __name__, xyzfilename).decode())
         af = AIMDFragmentation(cutoff=6.0, xyzfilename=xyzfilename)
         af.run()
         for filename in (af.outputenergyfile, af.outputfile):
             with open(filename) as f:
                 logging.info(filename)
                 print(f.read())
-
-
-if __name__ == '__main__':
-    unittest.main()
